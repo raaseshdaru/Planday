16d43a95b7056f4ffe940fbba5991da376904cbd
raaseshdaru <raasesh.daru@yahoo.com>
2022-06-20 00:51:05 +0200

    Updated test cases with descriptions. Added variables for page objects. Assertions included in all tests.

--- src/test/java/plandaytest/Testcase.java
+++ src/test/java/plandaytest/Testcase.java
@@ -13,191 +13,126 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Testcase {
-	
-	
+
 	ChromeDriver driver;
 	
-	private void handleLogin() {
-		driver.findElement(By.id("Username")).sendKeys("plandayqa@outlook.com");
-		driver.findElement(By.id("Password")).sendKeys("APItesting21"); 
-	    driver.findElement(By.id("MainLoginButton")).click();  
-	}
-	@BeforeMethod
+	//Page locators
+
+	public static final String TEST_URL = "https://test1234.planday.com/";
+	By usernameLocator = By.id("Username");
+	By passwordLocator = By.id("Password");
+	By loginButtonLocator = By.id("MainLoginButton");
+	By scheduleLocator = By.xpath("//a[@href='/schedule']");
+	By validationMessageLocator = By.className("validation-message");
+	By iframeLocator = By.xpath("//body/div[@id='root']/div[1]/iframe[1]");
+	By employeeCountLocator = By.xpath("//*[@class='row-header3']");
+	By nextWeekInterval = By.xpath("//*[@class='button--right date-bar__button']");
+	By todayButtonLocator = By.xpath("//button[text()='Today']");
+	By addShiftLocator =  By.xpath("//div[contains(@class,'virtualized-board__row')][2]/div[contains(@class,'board__cell')][7]");
+	By shiftStartTime = By.id("shiftStartEnd_start");
+	By shiftEndTime = By.id("shiftStartEnd_end");
+	By createButtonLocator = By.xpath("/html/body/div[6]/div/div/div/div[6]/div/div/ul/li[2]/button");
+	By shiftCreatedLocator = By.xpath("//div[contains(@class,'virtualized-board__row')][2]");
 	
-public void setUp() {
-		
-	WebDriverManager.chromedriver().setup();
-	driver = new ChromeDriver();
-	driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-	driver.get("https://test1234.planday.com/");
-	driver.findElement(By.className("cookie-banner__button")).click();
-		
-				
+	//User login method
+	private void handleLogin() {
+		driver.findElement(usernameLocator).sendKeys("plandayqa@outlook.com");
+		driver.findElement(passwordLocator).sendKeys("APItesting21");
+		driver.findElement(loginButtonLocator).click();
+	}
+
+	@BeforeMethod
+
+	public void setUp() {
+
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get(TEST_URL);
+		driver.findElement(By.className("cookie-banner__button")).click();
+
 	}
 
 	@AfterMethod
-	
-	public void tearDown()
-	{	
+
+	public void tearDown() {
 		driver.quit();
-		driver=null;
+
 	}
-	
 
+	@Test(description = "Test 1 Verifies if username, password and login button fields are displayed")
+	public void VerifyLoginFieldsAvailability() {
 
-	@Test
-	public void test1NavigateandVeryfy() 
-	{
-		
-		
-		driver.findElement(By.xpath("//*[@id=\'Username\']")).isDisplayed();
-		driver.findElement(By.xpath("//*[@id='Password']")).isDisplayed();
-		driver.findElement(By.xpath("//*[@id=\'MainLoginButton\']")).isDisplayed();
-        
-        
+		Assert.assertTrue(driver.findElement(usernameLocator).isDisplayed());
+		Assert.assertTrue(driver.findElement(passwordLocator).isDisplayed());
+		Assert.assertTrue(driver.findElement(loginButtonLocator).isDisplayed());
+
 	}
-		
 
-	@Test
-	public void test2VerifyError()
-	{
-		
-		
-		driver.findElement(By.id("Username")).sendKeys("xyz@gmail.com");
-		driver.findElement(By.id("Password")).sendKeys("1234"); 
-		driver.findElement(By.id("MainLoginButton")).click();
+	@Test(description = "Test 2 Verify if invalid login credentials produces validation message")
+	public void VerifyIncorrectLoginCredentialsError() {
 
-		if(driver.getPageSource().contains("The username or password is incorrect."))
-		{
-			System.out.println("Text is present");
-			}
-		else
-			{
-			System.out.println("Text is absent");
-			}
-		
-}	
-	
-	
-	@Test
-	public void test3Login()
-	{
-		handleLogin();	
+		boolean isValidationMessageDisplayed = false;
+		driver.findElement(usernameLocator).sendKeys("ppp@gmail.com");
+		driver.findElement(passwordLocator).sendKeys("2323");
+		driver.findElement(loginButtonLocator).click();
+
+		if (driver.findElements(validationMessageLocator).size() != 0) {
+			isValidationMessageDisplayed = true;
+		}
+		Assert.assertTrue(isValidationMessageDisplayed);
+
 	}
-	
-	
-	@Test
-	public void test4and5NavigateShcedulepageandverify()
-	{
-				
+
+	@Test(description = "Test 3 Verify successful login")
+	public void VerifyLogin() {
 		handleLogin();
-		
-		driver.findElement(By.xpath("//a[@href='/schedule']")).click();
-		
-		
-	    driver.manage().window().maximize();
-	    String baseUrl="https://test1234.planday.com/scdedule";
-	    driver.get(baseUrl);
-	    driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-	   
-	    
-	    //Assert.assertEquals(driver.getCurrentUrl(), baseUrl);
-	    if(baseUrl.equals(driver.getCurrentUrl()))
-	    {
-	       System.out.println("URLS Verify");
-	    }
-	    else
-	    {
-	       System.out.println("URLS not Verify");
-	   }
-		
+		Assert.assertEquals(driver.getTitle(), "Planday");
 	}
-	
-	
-	
-	
-	
-	@Test
-	public void test6countandassertnumberofemployee()
-	{
-		
-		
-		handleLogin();
-		
-		driver.findElement(By.xpath("//a[@href='/schedule']")).click();
-		
-		WebElement iframe = driver.findElement(By.xpath("//body/div[@id='root']/div[1]/iframe[1]"));
 
-	    driver.switchTo().frame(iframe);
-	      
-	    int size = driver.findElements(By.xpath("//*[@class='row-header3']")).size();
-	    Assert.assertEquals((size-1), 3);
-	    
-	    driver.findElement(By.xpath("//*[@class='button--right date-bar__button']")).click();
-		
-	}
-	
-	
-	
-	@Test
-	public void test7and8createashiftandfilltime()
-	{
-		
-		
+	@Test(description = "Test 4,5 Verify click on schdeule redirects to the correct /schedule page")
+	public void VerifyRedirectUrlContainsSchedule() {
 		handleLogin();
-		
-		driver.findElement(By.xpath("//a[@href='/schedule']")).click();
-		
-		WebElement iframe = driver.findElement(By.xpath("//body/div[@id='root']/div[1]/iframe[1]"));
-		driver.switchTo().frame(iframe);
-	
-	      
-	    int size = driver.findElements(By.xpath("//*[@class='row-header3']")).size();
-	    Assert.assertEquals((size-1), 3);
-	    
-	    driver.findElement(By.xpath("//*[@class='button--right date-bar__button']")).click();
-		
-		driver.findElement(By.xpath("//button[text()='Today']")).click();
-		driver.findElement(By.xpath("//div[contains(@class,'virtualized-board__row')][2]/div[contains(@class,'board__cell')][7]")).click();
-		driver.findElement(By.id("shiftStartEnd_start")).sendKeys("9:00");
-		driver.findElement(By.id("shiftStartEnd_end")).sendKeys("17:00");
-		driver.findElement(By.xpath("/html/body/div[6]/div/div/div/div[6]/div/div/ul/li[2]/button")).click();
-		
-		
+		driver.findElement(scheduleLocator).click();
+		Assert.assertTrue(driver.getCurrentUrl().contains("/schedule"));
 	}
-	
-	
-	
-	
-	@Test
-	public void test9assertcreatedshift()
-	{
-		
+
+	@Test(description = "Test 6 Count and seert that the number of employees displayed is 3")
+	public void CountAndAssertNumberOfEmployees() {
+
 		handleLogin();
-		
-		driver.findElement(By.xpath("//a[@href='/schedule']")).click();
-		
-		WebElement iframe = driver.findElement(By.xpath("//body/div[@id='root']/div[1]/iframe[1]"));
+		driver.findElement(scheduleLocator).click();
+		WebElement iframe = driver.findElement(iframeLocator);
 		driver.switchTo().frame(iframe);
 
-	    
-		WebElement ele = driver.findElement(By.xpath("//div[contains(@class,'virtualized-board__row')][2]"));
-	
-		
-		if (ele.getText().contains("Shift"))
-		{
-			  System.out.println("Shift Created");
-	    }
-	    else
-	    {
-	       System.out.println("Shift not Created");
-	   }
-		        
-		
+		int size = driver.findElements(employeeCountLocator).size();
+		Assert.assertEquals((size - 1), 3);
+
 	}
-	
+
+	@Test(description = "Test 7,8, 9 Verify shift created is visible on the schedule grid")
+	public void VerifyShiftShownOnScheduleGrid() {
+
+		handleLogin();
+
+		driver.findElement(scheduleLocator).click();
+
+		WebElement iframe = driver.findElement(iframeLocator);
+		driver.switchTo().frame(iframe);
+
+		driver.findElement(nextWeekInterval).click();
+
+		driver.findElement(todayButtonLocator).click();
+		driver.findElement(addShiftLocator).click();
+		driver.findElement(shiftStartTime).sendKeys("9:00");
+		driver.findElement(shiftEndTime).sendKeys("17:00");
+		driver.findElement(createButtonLocator).click();
+		WebElement ele = driver.findElement(shiftCreatedLocator);
+		Assert.assertTrue(ele.getText().contains("Shift"));
+
+	}
+
 }
